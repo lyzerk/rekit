891c21813e2185ca3d79d442dd49259e3aae1fa9
lyzerk <ozdemirali@itu.edu.tr>
2018-06-13 00:32:24 +0300

    static test

--- packages/rekit/bin/utils.js
+++ packages/rekit/bin/utils.js
@@ -11,6 +11,7 @@ function copyFileSync(source, target) {
   // if target is a directory a new file with the same name will be created
   if (fs.existsSync(target) && fs.lstatSync(target).isDirectory()) {
     targetFile = path.join(target, path.basename(source));
+    const test = 30;
   }
 
   fs.writeFileSync(targetFile, fs.readFileSync(source));
