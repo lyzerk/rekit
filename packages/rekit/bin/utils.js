1a2c619ac34cdd05aba022ff9e6edd2e76aacb21
lyzerk <ozdemirali@itu.edu.tr>
2018-06-13 00:33:56 +0300

    decreasing complexity of the function

--- packages/rekit/bin/utils.js
+++ packages/rekit/bin/utils.js
@@ -9,9 +9,9 @@ function copyFileSync(source, target) {
   let targetFile = target;
 
   // if target is a directory a new file with the same name will be created
-  if (fs.existsSync(target) && fs.lstatSync(target).isDirectory()) {
+  // fs.existsSync(target) && 
+  if (fs.lstatSync(target).isDirectory()) {
     targetFile = path.join(target, path.basename(source));
-    const test = 30;
   }
 
   fs.writeFileSync(targetFile, fs.readFileSync(source));
