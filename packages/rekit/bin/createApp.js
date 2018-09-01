48b72d52c67bc8a74d0f94377e378997e38e6834
lyzerk <ozdemirali@itu.edu.tr>
2018-09-01 22:49:03 +0300

    changing test variable

--- packages/rekit/bin/createApp.js
+++ packages/rekit/bin/createApp.js
@@ -18,7 +18,7 @@ function createApp(args) {
     console.log('Error: please specify the project name.');
     process.exit(1);
   }
-  const test = 10;
+  const test = 2;
   // The created project dir
   const prjPath = path.join(process.cwd(), prjName);
   if (fs.existsSync(prjPath)) {
